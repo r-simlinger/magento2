020d977f39d89a869c43b0730a3f612514e4e8c0
Rico Simlinger <r.simlinger@secupay.ag>
2018-06-10 09:52:38 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/etc/init.d/mysql start
-/etc/init.d/php7.1-fpm start
-/etc/init.d/nginx start
+/etc/init.d/mysql restart
+/etc/init.d/php7.1-fpm restart
+/etc/init.d/nginx restart
 while true; do sleep 1d; done
